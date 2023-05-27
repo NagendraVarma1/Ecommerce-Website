dd7d32b9ce1700cd8995e321fdfee1f5854ed119
Nagendravarma1 <nagendravarmal512@gmail.com>
2023-05-27 13:17:43 +0530

    task 1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import NavBar from "./Components/NavBar/NavBar";
+import Header from "./Components/Header/Header";
+import ProductList from "./Components/ProductList/ProductList";
+import Footer from "./Components/Footer/Footer";
+
+const App = () => {
+  return (
+    <>
+      <NavBar />
+      <Header />
+      <ProductList />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
